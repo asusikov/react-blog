aecc552c452b63c4175b9fad040d43803bd3edfc
asusikov <susikov.alexander@gmail.com>
2018-01-13 21:46:33 +0300

    Refactored LikeButton component and add action to BlogPage container

--- components/containers/BlogPage.jsx
+++ components/containers/BlogPage.jsx
@@ -4,11 +4,27 @@ import BlogList from '../ui/BlogList';
 const { Component } = React;
 
 class BlogPage extends Component {
+  constructor(props) {
+    super(...arguments);
+    this.state = {
+      items: props.items.map((item) => {
+        item.numberOfLikes = item.numberOfLikes || 0;
+        return item;
+      })
+    };
+    this.likePost = this.likePost.bind(this);
+  }
+  likePost(postId) {
+    this.setState((prevState) => prevState.items.map((item) => {
+      if (item.id === postId) item.numberOfLikes++;
+      return item;
+    }));
+  }
   render() {
     return (
       <div>
         <h1>Awesome React Blog!</h1>
-        <BlogList items={this.props.items}/>
+        <BlogList items={this.state.items} likePost={this.likePost}/>
       </div>
     );
   }
